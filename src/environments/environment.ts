641e76c3cb7b7284a4e5fa09c43082980d5cc988
Tô Hải Đăng <tohaidang161@gmail.com>
2025-07-24 17:27:35 +0700

    ignore env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   apiKey:
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTM3NmU5YTcxZjcyOTFhMGI1ZTU0MDExNzQ4ZDE0ZiIsIm5iZiI6MTc1MzA3ODMwMi45NDksInN1YiI6IjY4N2RkYTFlNzMwYWI1NzJiNjhhNGM4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MgZfjniR7Vlqo042maOwYgUENIhrLYAhgUgyJeiIJt4',
-  baseUrl: 'https://image.tmdb.org/t/p/',
+  baseUrlImg: 'https://image.tmdb.org/t/p/',
+  baseUrlMovie: 'https://api.themoviedb.org/3/movie/',
   production: false,
 };
