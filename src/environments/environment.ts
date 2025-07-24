92c955102b89a205056d2d4aed781489d13f41ff
Tô Hải Đăng <tohaidang161@gmail.com>
2025-07-24 11:45:00 +0700

    fade out scroll bar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  apiKey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTM3NmU5YTcxZjcyOTFhMGI1ZTU0MDExNzQ4ZDE0ZiIsIm5iZiI6MTc1MzA3ODMwMi45NDksInN1YiI6IjY4N2RkYTFlNzMwYWI1NzJiNjhhNGM4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MgZfjniR7Vlqo042maOwYgUENIhrLYAhgUgyJeiIJt4',
+  baseUrl: 'https://image.tmdb.org/t/p/',
+  production: false,
 };
 
 /*
