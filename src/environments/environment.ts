2b3d8ff72790914822cb5838060c077c8f961ca9
Tô Hải Đăng <tohaidang161@gmail.com>
2025-07-24 17:30:23 +0700

    Remove src/environments from tracking

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  apiKey:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTM3NmU5YTcxZjcyOTFhMGI1ZTU0MDExNzQ4ZDE0ZiIsIm5iZiI6MTc1MzA3ODMwMi45NDksInN1YiI6IjY4N2RkYTFlNzMwYWI1NzJiNjhhNGM4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MgZfjniR7Vlqo042maOwYgUENIhrLYAhgUgyJeiIJt4',
-  baseUrlImg: 'https://image.tmdb.org/t/p/',
-  baseUrlMovie: 'https://api.themoviedb.org/3/movie/',
-  production: false,
-};
