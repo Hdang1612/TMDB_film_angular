8b14c4b0f8e0ad392eab9d6bb12654fb7939793a
Tô Hải Đăng <tohaidang161@gmail.com>
2025-08-12 09:32:55 +0700

    config deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTM3NmU5YTcxZjcyOTFhMGI1ZTU0MDExNzQ4ZDE0ZiIsIm5iZiI6MTc1MzA3ODMwMi45NDksInN1YiI6IjY4N2RkYTFlNzMwYWI1NzJiNjhhNGM4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MgZfjniR7Vlqo042maOwYgUENIhrLYAhgUgyJeiIJt4',
+  baseUrlImg: 'https://image.tmdb.org/t/p/',
+  baseUrlMovie: 'https://api.themoviedb.org/3/',
+  baseUrlTrailer: 'https://www.youtube.com/embed/',
+  accountId: 22161944,
+  tempFemaleUserUrlImg:
+    'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-36-user-female-grey-d9222f16ec16a33ed5e2c9bbdca07a4c48db14008bbebbabced8f8ed1fa2ad59.svg',
+  tempMaleUserUrlImg:
+    'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg',
 };
